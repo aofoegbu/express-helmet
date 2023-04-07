f7894dea7beb76a7d178175bf4a08c09bf39b583
Augustine Ofoegbu <augustineofoegbu1@gmail.com>
2023-04-07 09:20:31 -0700

    Helmetjs for Header security in Express js apps

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const helmet = require("helmet");
+
+// creating an Express instance
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// setting "Referrer-Policy" to "no-referrer"
+app.use(
+  helmet.hsts({
+    // 60 days
+    maxAge: 86400,
+    // removing the "includeSubDomains" option
+    includeSubDomains: false,
+  })
+);
+
+// initializing a basic API that
+// returns the "Hello, World!" message
+app.get("/", (req, res) => {
+  res.json("Hello, World!");
+});
+
+// running the server
+app.listen(PORT, () => {
+  console.log(`Starting Express server on http://localhost:${PORT}`);
+});
